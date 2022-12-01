094ca368d758a636c2786319d86fa4036089cbfd
Joel Lake <joeldaniellake@gmail.com>
2022-12-01 11:26:04 -0330

    Part two complete

--- day1/day1.py
+++ day1/day1.py
@@ -2,5 +2,8 @@ elfSnacks = '/Users/joellake/Documents/GitHub/advent-of-code-2022/day1/input.txt
 allTheElves = open(elfSnacks, 'r').read().split('\n\n'); 
 hungriestElfList = []
 for elf in allTheElves: hungriestElfList.append(sum([int (elf) for elf in elf.split("\n") if elf]))
+# Part One
 print(max(hungriestElfList))
+# Part Two
+print(sum(sorted(hungriestElfList, reverse=True)[:3]))
 
